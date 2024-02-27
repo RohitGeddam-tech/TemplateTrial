339a96abb725ee706f0b81c88152bea8e4144dd6
Kiran Patil <kiranpatil167@gmail.com>
2024-02-27 13:20:43 +0530

    formatted text function error resolved

--- utils/useFormattedText.js
+++ utils/useFormattedText.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export const useFormattedText = (title) => {
   let str = title;
-  const formattedText = str.replace("&nbsp;", "\u00a0").split("\n");
+  const formattedText = str?.replace("&nbsp;", "\u00a0").split("\n");
 
   return (
     <>
-      {formattedText.map((line, index) => (
+      {formattedText?.map((line, index) => (
         <React.Fragment key={index}>
           {line}
           {index < formattedText.length - 1 && <br />}
